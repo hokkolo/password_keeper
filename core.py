c6cea472e80d5bb9edae6c35ae6b195e868949f9
hokkolo <hokkolo@gmail.com>
2020-04-11 12:20:13 +0530

    Staging part 3 edit correction

--- core.py
+++ core.py
@@ -184,11 +184,10 @@ def edit():
      conn = connection(db)
      handle = conn.cursor()
      sch = input("Enter the tag to edit: ")
-     new_tag = input("Enter the new Tag name: ")
      new_uname = input("Enter new Username: ")
      new_pword = input("Enter new Password: ")
-     new_data = (new_tag,new_uname,new_pword,sch)
-     sql_query = """update data set tag = ?, set uname = ?, set pswd = ? where tag = ?"""
+     new_data = (new_uname,new_pword,sch)
+     sql_query = """update data set uname = ?, set pswd = ? where tag = ?"""
      handle.execute(sql_query,new_data)
      print("Record Updated Successfully")
      handle.close()
